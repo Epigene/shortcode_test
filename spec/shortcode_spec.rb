d960cf97d5777fffc962123159439be3d81ff6db
Epigene <augusts.bautra@gmail.com>
2015-09-14 15:16:42 +0300

    Init

---
+++ spec/shortcode_spec.rb
@@ -0,0 +1,11 @@
+# rspec spec/shortcode_spec.rb
+require 'rails_helper'
+
+describe Shortcode do
+  it "works for first, short template name" do
+    expect(1).to eq 1
+  end
+  it "does not work for the latter, longer template" do
+    expect(1).to eq 1
+  end
+end
